b4f454b8b1fcfc3cd834725670e7e70666f87185
Evan <e.godon@outlook.com>
2016-12-15 22:08:06 -0500

    Added browser support fallbacks

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,38 +1,38 @@
 import $ from 'jquery';
 
 class Modal {
-	constructor(){
-		this.openModalButton = $('.open-modal');
-		this.modal = $(".modal");
-		this.closeModalButton = $(".modal__close");
-		this.events();
-	}
+  constructor() {
+    this.openModalButton = $(".open-modal");
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal__close");
+    this.events();
+  }
 
-	events(){
-		//clicking the open modal button
-		this.openModalButton.click(this.openModal.bind(this));
-		
-		//clicking the close modal button
-		this.closeModalButton.click(this.closeModal.bind(this));
-		
-		//pushes any key
-		$(document).keyup(this.keyPressHandler.bind(this));
-	}
+  events() {
+    // clicking the open modal button
+    this.openModalButton.click(this.openModal.bind(this));
 
-	keyPressHandler(e){
-		if (e.keyCode == 27){
-			this.closeModal();
-		}
-	}
+    // clicking the x close modal button
+    this.closeModalButton.click(this.closeModal.bind(this));
 
-	openModal(){
-		this.modal.addClass("modal--is-visible");
-		return false;
-	}
+    // pushes any key
+    $(document).keyup(this.keyPressHandler.bind(this));
+  }
 
-	closeModal(){
-		this.modal.removeClass("modal--is-visible");
-	}
+  keyPressHandler(e) {
+    if (e.keyCode == 27) {
+      this.closeModal();
+    }
+  }
+
+  openModal() {
+    this.modal.addClass("modal--is-visible");
+    return false;
+  }
+
+  closeModal() {
+    this.modal.removeClass("modal--is-visible");
+  }
 }
 
 export default Modal;
